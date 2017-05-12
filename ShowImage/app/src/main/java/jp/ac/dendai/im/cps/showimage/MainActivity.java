95fa288d477f9e2f786ce7a2903a06e9ec143641
akameco <akameco.t@gmail.com>
2017-05-13 06:14:45 +0900

    Add ShowImage

---
+++ ShowImage/app/src/main/java/jp/ac/dendai/im/cps/showimage/MainActivity.java
@@ -0,0 +1,20 @@
+package jp.ac.dendai.im.cps.showimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// 画像の表示については以下のサイトが参考になります
+// https://akira-watson.com/android/imageview.html
+// 今回は以下の手順で行いました
+// 1) res/drawableディレクトリの中にface.pngを配置
+// 2) activity_main.xmlにImageViewを配置
+// 3) ImageViewにandroid:src="@drawable/face"を指定
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
